e2195255ab250923d22025b7678e7955ee546143
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-01-11 19:04:22 -0300

    caractere em C# pelo Coddy

---
+++ caractere_Coddy/caractere/caractere/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Type your code below
+        char initial = 'C';
+
+        // Don't change the line below
+        System.Console.WriteLine("initial = " + initial);
+    }
+}
