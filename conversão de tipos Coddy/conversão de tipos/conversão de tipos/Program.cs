e47f55e3d5842cdb7d74d0af373e4ae87c816e82
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-01-22 21:47:29 -0300

    Conversão de tipos_Coddy

---
+++ conversão de tipos Coddy/conversão de tipos/conversão de tipos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Declare and initialize variables
+        double price = 99.99;
+        int intPrice = (int)price;
+
+        // Output the values
+        System.Console.WriteLine("Price: " + price);
+        System.Console.WriteLine("Int Price: " + intPrice);
+
+    }
+}
