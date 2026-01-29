159f4fbb076d7052d360d201ad9e4cebd9cef4c9
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-01-29 19:48:07 -0300

    Operadores lóficos 1 Coddy

---
+++ Operadores lógicos 1 Coddy/Operadores lógicos Coddy/Operadores lógicos Coddy/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Type your code below
+        bool b1 = 2 > 1;
+        bool b2 = 3 < 6;
+        bool b3 = b1 || b2;
+
+        // Don't change the line below
+        System.Console.WriteLine("b3 = " + b3);
+    }
+}
