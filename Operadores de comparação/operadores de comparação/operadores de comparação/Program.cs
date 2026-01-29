9f9647d7f800edd8dc67a20688737ef4c86badc5
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-01-28 21:26:17 -0300

    Operadores de comparação Coddy

---
+++ Operadores de comparação/operadores de comparação/operadores de comparação/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Type your code below
+        var n1 = 8;
+        var n2 = 9;
+        var n3 = n1 >= n2;
+
+        // Don't change the line below
+        System.Console.WriteLine("n1 = " + n1 + ", n2 = " + n2 + ", n3 = " + n3);
+    }
+}
