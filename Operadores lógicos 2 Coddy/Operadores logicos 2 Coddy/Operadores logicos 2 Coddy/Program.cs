9dbeaf53240981f295cc8c5106d7753f9aa03847
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-01-31 23:59:46 -0300

    Operadores logicos 2 Coddy

---
+++ Operadores lógicos 2 Coddy/Operadores logicos 2 Coddy/Operadores logicos 2 Coddy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Type your code below
+        int b1 = 2;
+        int b2 = 3;
+        bool b3 = !((b1 + b2) > (b1 * b2));
+
+        // Don\'t change the line below
+        Console.WriteLine("b3 = " + b3);
+    }
+}
