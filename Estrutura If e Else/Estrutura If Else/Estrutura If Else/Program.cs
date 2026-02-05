f80e52cfebdd74a2d5995e3eabeaa71fe6839cff
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-02-05 20:38:05 -0300

    Estrutura If Else Coddy

---
+++ Estrutura If e Else/Estrutura If Else/Estrutura If Else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a velocidade dos ventos");
+        int wind = int.Parse(Console.ReadLine()); // Don't change this line
+        string status = "unset";
+        // Type your code below
+        if (wind < 8)
+        {
+            status = "Calm";
+        }
+        else if (wind >= 8 && wind < 31)
+        {
+            status = "Breeze";
+        }
+        else if (wind > 32 && wind < 63)
+        {
+            status = "Gale";
+        }
+        else
+        {
+            status = "Storm";
+        }
+
+        // Don\'t change the line below
+        Console.WriteLine("status = " + status);
+    }
+}
